140509142ebca0c48847ec69fc56cfd751c4ade7
zivdamir <73081641+zivdamir@users.noreply.github.com>
2022-04-19 12:57:03 +0300

    pretty pretty
    more pretty

--- ex1.asm
+++ ex1.asm
@@ -6,11 +6,15 @@ _start: #change to _start before hagasha
 movl $64,%ecx # for looping 
 movq (num),%rax # we will rotate rax
 movl $0x0,%edx #rdx is our count.
-hw1_loop: cmp $0x0,%ecx
+
+hw1_loop:
+cmp $0x0,%ecx
 jz end
 dec %ecx #count --
 shl %rax# rotate one time through carry left 
 jnc hw1_loop # if carry isn't one, our lsb isn't one,meaning we dont need to do anything
 incl %edx
 jmp hw1_loop
-end:movl %edx,(CountBits)
+
+end:
+movl %edx,(CountBits)
