3f9c03121cefa33bd9a5ee64a2cf8f4ade81ae18
zivdamir <73081641+zivdamir@users.noreply.github.com>
2022-04-19 15:35:02 +0300

    _HW1

--- ex2.asm
+++ ex2.asm
@@ -4,14 +4,14 @@
 _start:
 movl (num),%eax
 cmp $0x0,%eax
-je end_hw1
+je end_HW1
 cmp $0x0,%eax
-js Dest_To_source_hw1
+js Dest_To_source_HW1
 leaq (source),%rcx
 leaq (destination),%rdx
-jmp overlap_hw1
+jmp overlap_HW1
 
-Dest_To_source_hw1:
+Dest_To_source_HW1:
 # our number is negative
 cdqe
 not %rax
@@ -21,36 +21,36 @@ leaq (destination),%rcx
 leaq (source),%rdx
 #if "source" is before "destination"
 
-overlap_hw1:
+overlap_HW1:
 cmp %rdx,%rcx # we want to check if adress of source is before address of destination
-jb give_offset_hw1
-jmp move_bytes_regular_hw1
+jb give_offset_HW1
+jmp move_bytes_regular_HW1
 
-give_offset_hw1:
+give_offset_HW1:
 add %rax,%rcx
 add %rax,%rdx
 decq %rcx
 decq %rdx
-jmp move_bytes_opposite_hw1
+jmp move_bytes_opposite_HW1
 
-move_bytes_regular_hw1:
+move_bytes_regular_HW1:
 cmp $0x0,%rax
-je end_hw1
+je end_HW1
 movb (%rcx),%r8b
 movb %r8b,(%rdx)
 inc %rcx
 inc %rdx
 decq %rax
-jmp move_bytes_regular_hw1
+jmp move_bytes_regular_HW1
 
-move_bytes_opposite_hw1:
+move_bytes_opposite_HW1:
 cmp $0x0,%rax
-je end_hw1
+je end_HW1
 movb (%rcx),%r8b
 movb %r8b,(%rdx)
 decq %rcx
 decq %rdx
 decq %rax
-jmp move_bytes_opposite_hw1
+jmp move_bytes_opposite_HW1
 
-end_hw1:
+end_HW1:
