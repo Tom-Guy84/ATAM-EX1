61a0d565ed28ac30452328a6d8d1c8fa73e61872
zivdamir <73081641+zivdamir@users.noreply.github.com>
2022-04-19 15:33:33 +0300

    ASD

--- ex1.asm
+++ ex1.asm
@@ -7,14 +7,14 @@ movl $64,%ecx # for looping
 movq (num),%rax # we will rotate rax
 movl $0x0,%edx #rdx is our count.
 
-hw1_loop:
+loop_HW1:
 cmp $0x0,%ecx
-jz end
+jz end_HW1
 dec %ecx #count --
 shl %rax# rotate one time through carry left 
-jnc hw1_loop # if carry isn't one, our lsb isn't one,meaning we dont need to do anything
+jnc loop_HW1 # if carry isn't one, our lsb isn't one,meaning we dont need to do anything
 incl %edx
-jmp hw1_loop
+jmp loop_HW1
 
-end:
+end_HW1:
 movl %edx,(CountBits)
